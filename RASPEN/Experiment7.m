883b130da43cb77779f0129bee9ebc2096c6ed84
Conor Mccoid <mccoid@matlnx4.unige.ch>
2018-11-13 17:17:01 +0100

    Exp7, first algo., playing around with how to solve the nonlinear system

--- RASPEN/Experiment7.m
+++ RASPEN/Experiment7.m
@@ -180,45 +180,42 @@ tol = 1e-8;
 error0 = zeros(itermax+1,1);
 error0(1) = 1;
 error1 = error0;
-nonlinsolves = 1;
+nonlinsolves = 10;
 
 % Standard additive Schwarz
 u2b = -b;
-u2bold = -b;
 iter = 0;
 u1 = -x1; u1(1) = 1;
 u2 = -x2; u2(end) = -1;
+u1old = u1;
+u2old = u2;
 while error0(iter+1) > tol && iter < itermax
     iter = iter+1;
 
-    % Step 1: solve u in first domain
     u1(end) = u2b;
-    for j = 1:nonlinsolves
-        f1 = -D1 * u1 + d1 * (u1.^2 / 2);
+        % Step 1: solve u in first domain
+        f1 = -D1 * u1 + u1 .* ( d1 * u1 );
         JJ = spdiags([u1(2:end-1) , -u1(3:end)+u1(1:end-2) , -u1(2:end-1)],[-1,0,1],l1-2,l1);
         JJ = JJ(:,2:end-1);
         JJ = J1 + JJ*(nx-1)/2;
-%             u1(2:end-1) = u1(2:end-1) + (JJ \ f1(2:end-1));
-        JJ = JJ \ eye(l1-2);
+        JJ = JJ \ eye(l1-2); S1 = JJ(x1(2:end-1)==a,end);
         u1(2:end-1) = u1(2:end-1) + JJ * f1(2:end-1);
-    end
-    u1a = u1(x1==a);
-
-    % Step 2: solve u in second domain
-    u2(1) = u1a;
-    for j = 1:nonlinsolves
-        f2 = -D2 * u2 + d2 * (u2.^2/2);
+        
+        u1a = u1(x1==a);
+        u2(1) = u1a;
+        
+        % Step 2: solve u in second domain
+        f2 = -D2 * u2 + u2 .* ( d2 * u2 );
         JJ = spdiags([u2(2:end-1) , -u2(3:end)+u2(1:end-2) , -u2(2:end-1)],[-1,0,1],l2-2,l2);
         JJ = JJ(:,2:end-1);
         JJ = J2 + JJ*(nx-1)/2;
-%             u2(2:end-1) = u2(2:end-1) + (JJ \ f2(2:end-1));
-        JJ = JJ \ eye(l2-2);
+        JJ = JJ \ eye(l2-2); S2 = JJ(x2(2:end-1)==b,1);
         u2(2:end-1) = u2(2:end-1) + JJ * f2(2:end-1);
-    end
     u2b = u2(x2==b);
 
-    error0(iter+1) = abs(u2b - u2bold);
-    u2bold= u2b;
+    error0(iter+1) = norm(u1 - u1old) + norm(u2 - u2old);
+    u1old = u1;
+    u2old = u2;
 
 end
 
@@ -231,70 +228,71 @@ u2bold = -b;
 iter = 0;
 u1 = -x1; u1(1) = 1;
 u2 = -x2; u2(end) = -1;
+u1old = u1;
+u2old = u2;
 while error1(iter+1) > tol && iter < itermax
-    iter = iter+1;
-
-    % Step 1: solve u in first domain
+    
     u1(end) = u2b;
-    for j = 1:nonlinsolves
+    errorA = 1;
+    iterA = iter;
+    while errorA > tol && iterA < itermax
+        iterA = iterA+1;
+        
+        % Step 1: solve u in first domain
         f1 = -D1 * u1 + u1 .* ( d1 * u1 );
         JJ = spdiags([u1(2:end-1) , -u1(3:end)+u1(1:end-2) , -u1(2:end-1)],[-1,0,1],l1-2,l1);
         JJ = JJ(:,2:end-1);
         JJ = J1 + JJ*(nx-1)/2;
-%             u1(2:end-1) = u1(2:end-1) + (JJ \ f1(2:end-1));
         JJ = JJ \ eye(l1-2); S1 = JJ(x1(2:end-1)==a,end);
         u1(2:end-1) = u1(2:end-1) + JJ * f1(2:end-1);
+        
+        errorA = norm(u1 - u1old);
+        u1old = u1;
+        
+        error1(iterA) = errorA;
     end
-    u1a = u1(x1==a);
-
-    % Step 2: solve u in second domain
-    u2(1) = u1a;
-    for j = 1:nonlinsolves
+        
+        u1a = u1(x1==a);
+        u2(1) = u1a;
+        
+    errorB = 1;
+    iterB = iter;
+    while errorB > tol && iterB < itermax
+        iterB = iterB+1;
+        
+        % Step 2: solve u in second domain
         f2 = -D2 * u2 + u2 .* ( d2 * u2 );
         JJ = spdiags([u2(2:end-1) , -u2(3:end)+u2(1:end-2) , -u2(2:end-1)],[-1,0,1],l2-2,l2);
         JJ = JJ(:,2:end-1);
         JJ = J2 + JJ*(nx-1)/2;
-%             u2(2:end-1) = u2(2:end-1) + (JJ \ f2(2:end-1));
         JJ = JJ \ eye(l2-2); S2 = JJ(x2(2:end-1)==b,1);
         u2(2:end-1) = u2(2:end-1) + JJ * f2(2:end-1);
+        
+        errorB = norm(u2 - u2old);
+        u2old = u2;
+        
+        error1(iterB) = error1(iterB) + errorB;
     end
+    iter = min(iterA,iterB);
     u2b = u2(x2==b);
-    
-    if nonlinsolves>1
-        
-        % Preconditioning with Newton
-        % Step 3: solve g in first domain
-        dF1 = diag(d1 * u1);
-        g1 = (D1 - dF1 - diag(u1)*d1) \ [ zeros(l1-1,1) ; 1 ];
+            
+    % Preconditioning with Newton
+    % Step 3: solve g in first domain
+    dF1 = diag(d1 * u1);
+    g1 = (D1 - dF1 - diag(u1)*d1) \ [ zeros(l1-1,1) ; 1 ];
 
-        % Step 4: solve g in second domain
-        dF2 = diag(d2 * u2);
-        g2 = (D2 - dF2 - diag(u2)*d2) \ [ g1(x1==a) ; zeros(l2-1,1) ];
+    % Step 4: solve g in second domain
+    dF2 = diag(d2 * u2);
+    g2 = (D2 - dF2 - diag(u2)*d2) \ [ g1(x1==a) ; zeros(l2-1,1) ];
 
-        % Step 5: update u2b
-        g2b = g2(x2==b);
-        u2b = ( g2b*u2bold - u2b )./ ( g2b - 1 );
-        
-    else
-    
-        % New preconditioning, for Newton instead of fixed point as
-        % nonlinear solver
-        g = -( -e*(nx-1)^2 + u1(end-1)*(nx-1)/2 ) * ( e*(nx-1)^2 + u2(2)*(nx-1)/2 ) * S1 * S2;
-%         u2b = u2bold - (u2b - u2bold)/(g - 1);
-        u2b = ( g*u2bold - u2b )./ (g - 1);
-        
-    end
-    
-    plot(iter,u2b,'b.')
-    hold on
+    % Step 5: update u2b
+    g2b = g2(x2==b);
+    u2b = ( g2b*u2bold - u2b )./ ( g2b - 1 );
 
-    error1(iter+1) = abs(u2b - u2bold);
+%     error1(iter+1) = abs(u2b - u2bold);
     u2bold= u2b;
 
 end
-
-pause
-hold off
 
 plot(x1,u1,x2,u2)
 pause

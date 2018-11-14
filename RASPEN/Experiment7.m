6d6e65fe428fc08755e3bf0602f685822a13279c
Conor Mccoid <mccoid@matlnx4.unige.ch>
2018-11-14 11:50:25 +0100

    Combining and examining errors related to ASPN and PNAS algorithms

--- RASPEN/Experiment7.m
+++ RASPEN/Experiment7.m
@@ -175,22 +175,24 @@ J2 = e * J2 * (nx-1)^2;
 J2 = spdiags(J2,[-1,0,1],l2-2,l2-2);
 
 % Initialization
-itermax = 100;
+u0 = -x;
+itermax = 200;
 tol = 1e-8;
-error0 = zeros(itermax+1,1);
-error0(1) = 1;
-error1 = error0;
-nonlinsolves = 10;
+error0_u = zeros(itermax+1,1);
+error0_u(1) = 1;
+error1_u = error0_u;
+error2_u = error0_u;
+error0_g = error0_u;
+error1_g = error0_u;
+error2_g = error0_u;
 
 % Standard additive Schwarz
 u2b = -b;
-iter = 0;
-u1 = -x1; u1(1) = 1;
-u2 = -x2; u2(end) = -1;
-u1old = u1;
-u2old = u2;
-while error0(iter+1) > tol && iter < itermax
-    iter = iter+1;
+u2bold = -b;
+u1 = u0(x<=b); u1(1) = 1;
+u2 = u0(x>=a); u2(end) = -1;
+uold = u0;
+for iter = 1:itermax
 
     u1(end) = u2b;
         % Step 1: solve u in first domain
@@ -212,25 +214,25 @@ while error0(iter+1) > tol && iter < itermax
         JJ = JJ \ eye(l2-2); S2 = JJ(x2(2:end-1)==b,1);
         u2(2:end-1) = u2(2:end-1) + JJ * f2(2:end-1);
     u2b = u2(x2==b);
-
-    error0(iter+1) = norm(u1 - u1old) + norm(u2 - u2old);
-    u1old = u1;
-    u2old = u2;
+    
+    u = [ u1(x1<a) ; 0.5*u1(x1>=a) + 0.5*u2(x2<=b) ; u2(x2>b) ];
+    
+    error0_u(iter) = norm(u - uold);
+    error0_g(iter) = abs(u2b - u2bold);
+    uold = u;
+    u2bold = u2b;
 
 end
 
-plot(x1,u1,x2,u2)
-pause
-
-% Preconditioning with Newton
+% Preconditioning with Newton (u then bc.)
 u2b = -b;
 u2bold = -b;
 iter = 0;
-u1 = -x1; u1(1) = 1;
-u2 = -x2; u2(end) = -1;
+u1 = u0(x<=b); u1(1) = 1;
+u2 = u0(x>=a); u2(end) = -1;
 u1old = u1;
 u2old = u2;
-while error1(iter+1) > tol && iter < itermax
+while iter < itermax
     
     u1(end) = u2b;
     errorA = 1;
@@ -249,7 +251,7 @@ while error1(iter+1) > tol && iter < itermax
         errorA = norm(u1 - u1old);
         u1old = u1;
         
-        error1(iterA) = errorA;
+        error1_u(iterA) = errorA;
     end
         
         u1a = u1(x1==a);
@@ -271,7 +273,7 @@ while error1(iter+1) > tol && iter < itermax
         errorB = norm(u2 - u2old);
         u2old = u2;
         
-        error1(iterB) = error1(iterB) + errorB;
+        error1_u(iterB) = error1_u(iterB) + errorB;
     end
     iter = min(iterA,iterB);
     u2b = u2(x2==b);
@@ -289,15 +291,73 @@ while error1(iter+1) > tol && iter < itermax
     g2b = g2(x2==b);
     u2b = ( g2b*u2bold - u2b )./ ( g2b - 1 );
 
-%     error1(iter+1) = abs(u2b - u2bold);
+    error1_g(iter+1) = abs(u2b - u2bold);
     u2bold= u2b;
 
 end
 
-plot(x1,u1,x2,u2)
-pause
+% Preconditioning with Newton (bc. then u)
+u2b = -b;
+u2bold = -b;
+u1 = u0(x<=b); u1(1) = 1;
+u2 = u0(x>=a); u2(end) = -1;
+u1old = u1;
+u2old = u2;
+uold = u0;
+for iter = 1:itermax
+        
+    u1 = u1old;
+    u1(end) = u2b;
 
-semilogy(1:(itermax+1),error0,'b*--',1:(itermax+1),error1,'r*--')
+    f1 = -D1 * u1 + u1 .* ( d1 * u1 );
+    JJ = spdiags([u1(2:end-1) , -u1(3:end)+u1(1:end-2) , -u1(2:end-1)],[-1,0,1],l1-2,l1);
+    JJ = JJ(:,2:end-1);
+    JJ = J1 + JJ*(nx-1)/2;
+    JJ = JJ \ eye(l1-2); S1 = JJ(x1(2:end-1)==a,end);
+    u1(2:end-1) = u1(2:end-1) + JJ * f1(2:end-1);
+
+    u1a = u1(x1==a);
+    u2  = u2old;
+    u2(1) = u1a;
+
+    f2 = -D2 * u2 + u2 .* ( d2 * u2 );
+    JJ = spdiags([u2(2:end-1) , -u2(3:end)+u2(1:end-2) , -u2(2:end-1)],[-1,0,1],l2-2,l2);
+    JJ = JJ(:,2:end-1);
+    JJ = J2 + JJ*(nx-1)/2;
+    JJ = JJ \ eye(l2-2); S2 = JJ(x2(2:end-1)==b,1);
+    u2(2:end-1) = u2(2:end-1) + JJ * f2(2:end-1);
+
+    u2b = u2(x2==b);
+
+    % New preconditioning, for Newton instead of fixed point as
+    % nonlinear solver
+    g = -( -e*(nx-1)^2 + u1(end-1)*(nx-1)/2 ) * ( e*(nx-1)^2 + u2(2)*(nx-1)/2 ) * S1 * S2;
+    u2b = ( g*u2bold - u2b )./ (g - 1);
+
+    error2_g(iter) = abs(u2b - u2bold);
+    u2bold = u2b;
+
+    if error2_g(iter) < tol
+
+        u = [ u1(x1<a) ; 0.5*u1(x1>=a) + 0.5*u2(x2<=b) ; u2(x2>b) ];
+
+        error2_u(iter) = norm(u - uold);
+        u1old = u1;
+        u2old = u2;
+        uold = u;
+
+    end
+
+end
+
+subplot(1,2,1)
+semilogy(1:(itermax+1),error0_u,'b*--',1:(itermax+1),error1_u,'r*--',1:(itermax+1),error2_u,'k*--')
+xlabel('Iteration')
+ylabel('2-norm of change in u')
+legend('AS','ASPN','PNAS')
+
+subplot(1,2,2)
+semilogy(1:(itermax+1),error0_g,'b*--',1:(itermax+1),error1_g,'r*--',1:(itermax+1),error2_g,'k*--')
 xlabel('Iteration')
 ylabel('Change in u_2(\beta)')
-legend('AS','ASPN')
+legend('AS','ASPN','PNAS')

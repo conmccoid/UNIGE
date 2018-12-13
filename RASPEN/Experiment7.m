6dd40193c3a604ce4d24cea9d45b502e853ac287
Conor Mccoid <mccoid@matlnx4.unige.ch>
2018-12-13 10:25:20 +0100

    Changed nx-1 to dx (to resolve minor issues), 13.12.18

--- RASPEN/Experiment7.m
+++ RASPEN/Experiment7.m
@@ -4,7 +4,7 @@
 e = 0.1;
 
 % Grid
-nx = 1001; dx = 1/(nx-1);
+nx = 1001; dx = 2/(nx-1);
 a  = -1/3;
 b  = 1/3;
 x  = linspace(-1,1,nx)';
@@ -135,11 +135,12 @@ title('Newton precond. on transmission condition')
 e = 0.1;
 
 % Grid
-nx = 1001; dx = 1/(nx-1);
+nx = 1001; dx = (nx - 1)/2;
 L = 0.2;
 a  = -L;
 b  = L;
 x  = linspace(-1,1,nx)';
+dx = 1./abs(x(2) - x(1));
 x1 = x(x<=b); b = x1(end);
 x2 = x(x>=a); a = x2(1);
 l1 = length(x1); l2 = length(x2);
@@ -147,32 +148,32 @@ l1 = length(x1); l2 = length(x2);
 % Diff. mat.
 I1 = eye(length(x1));
 D1 = ones(length(x1)-2,1);
-D1 = (nx-1)^2 * [ D1, -2*D1, D1 ];
+D1 = dx^2 * [ D1, -2*D1, D1 ];
 D1 = spdiags(D1,[0,1,2],l1-2,l1);
 D1 = [I1(1,:) ; e*D1 ; I1(end,:) ];
 I2 = eye(length(x2));
 D2 = ones(length(x2)-2,1);
-D2 = (nx-1)^2 * [ D2, -2*D2, D2 ];
+D2 = dx^2 * [ D2, -2*D2, D2 ];
 D2 = spdiags(D2,[0,1,2],l2-2,l2);
 D2 = [I2(1,:) ; e*D2 ; I2(end,:) ];
 
 d1 = ones(l1-2,1);
-d1 = (nx-1)/2 * [ -d1, zeros(l1-2,1), d1 ];
+d1 = dx/2 * [ -d1, zeros(l1-2,1), d1 ];
 d1 = spdiags(d1,[0,1,2],l1-2,l1);
 d1 = [ zeros(1,l1) ; d1 ; zeros(1,l1) ];
 d2 = ones(l2-2,1);
-d2 = (nx-1)/2 * [ -d2, zeros(l2-2,1), d2 ];
+d2 = dx/2 * [ -d2, zeros(l2-2,1), d2 ];
 d2 = spdiags(d2,[0,1,2],l2-2,l2);
 d2 = [ zeros(1,l2) ; d2 ; zeros(1,l2) ];
 
 % Jacobian
 J1 = ones(l1-2,1);
 J1 = [J1, -2*J1, J1];
-J1 = e * J1 * (nx-1)^2;
+J1 = e * J1 * dx^2;
 J1 = spdiags(J1,[-1,0,1],l1-2,l1-2);
 J2 = ones(l2-2,1);
 J2 = [J2, -2*J2, J2];
-J2 = e * J2 * (nx-1)^2;
+J2 = e * J2 * dx^2;
 J2 = spdiags(J2,[-1,0,1],l2-2,l2-2);
 
 % Initialization
@@ -200,7 +201,7 @@ for iter = 1:itermax
         f1 = -D1 * u1 + u1 .* ( d1 * u1 );
         JJ = spdiags([u1(2:end-1) , -u1(3:end)+u1(1:end-2) , -u1(2:end-1)],[-1,0,1],l1-2,l1);
         JJ = JJ(:,2:end-1);
-        JJ = J1 + JJ*(nx-1)/2;
+        JJ = J1 + JJ*dx/2;
         JJ = JJ \ eye(l1-2); S1 = JJ(x1(2:end-1)==a,end);
         u1(2:end-1) = u1(2:end-1) + JJ * f1(2:end-1);
         
@@ -211,7 +212,7 @@ for iter = 1:itermax
         f2 = -D2 * u2 + u2 .* ( d2 * u2 );
         JJ = spdiags([u2(2:end-1) , -u2(3:end)+u2(1:end-2) , -u2(2:end-1)],[-1,0,1],l2-2,l2);
         JJ = JJ(:,2:end-1);
-        JJ = J2 + JJ*(nx-1)/2;
+        JJ = J2 + JJ*dx/2;
         JJ = JJ \ eye(l2-2); S2 = JJ(x2(2:end-1)==b,1);
         u2(2:end-1) = u2(2:end-1) + JJ * f2(2:end-1);
     u2b = u2(x2==b);
@@ -247,7 +248,7 @@ while iter < itermax
         f1 = -D1 * u1 + u1 .* ( d1 * u1 );
         JJ = spdiags([u1(2:end-1) , -u1(3:end)+u1(1:end-2) , -u1(2:end-1)],[-1,0,1],l1-2,l1);
         JJ = JJ(:,2:end-1);
-        JJ = J1 + JJ*(nx-1)/2;
+        JJ = J1 + JJ*dx/2;
         JJ = JJ \ eye(l1-2); S1 = JJ(x1(2:end-1)==a,end);
         u1(2:end-1) = u1(2:end-1) + JJ * f1(2:end-1);
         
@@ -270,7 +271,7 @@ while iter < itermax
         f2 = -D2 * u2 + u2 .* ( d2 * u2 );
         JJ = spdiags([u2(2:end-1) , -u2(3:end)+u2(1:end-2) , -u2(2:end-1)],[-1,0,1],l2-2,l2);
         JJ = JJ(:,2:end-1);
-        JJ = J2 + JJ*(nx-1)/2;
+        JJ = J2 + JJ*dx/2;
         JJ = JJ \ eye(l2-2); S2 = JJ(x2(2:end-1)==b,1);
         u2(2:end-1) = u2(2:end-1) + JJ * f2(2:end-1);
         
@@ -317,7 +318,7 @@ for iter = 1:4*itermax
     f1 = -D1 * u1 + u1 .* ( d1 * u1 );
     JJ = spdiags([u1(2:end-1) , -u1(3:end)+u1(1:end-2) , -u1(2:end-1)],[-1,0,1],l1-2,l1);
     JJ = JJ(:,2:end-1);
-    JJ = J1 + JJ*(nx-1)/2;
+    JJ = J1 + JJ*dx/2;
     JJ = JJ \ eye(l1-2); S1 = JJ(x1(2:end-1)==a,end);
     u1(2:end-1) = u1(2:end-1) + JJ * f1(2:end-1);
 
@@ -328,7 +329,7 @@ for iter = 1:4*itermax
     f2 = -D2 * u2 + u2 .* ( d2 * u2 );
     JJ = spdiags([u2(2:end-1) , -u2(3:end)+u2(1:end-2) , -u2(2:end-1)],[-1,0,1],l2-2,l2);
     JJ = JJ(:,2:end-1);
-    JJ = J2 + JJ*(nx-1)/2;
+    JJ = J2 + JJ*dx/2;
     JJ = JJ \ eye(l2-2); S2 = JJ(x2(2:end-1)==b,1);
     u2(2:end-1) = u2(2:end-1) + JJ * f2(2:end-1);
 
@@ -336,7 +337,7 @@ for iter = 1:4*itermax
 
     % New preconditioning, for Newton instead of fixed point as
     % nonlinear solver
-    g = -( -e*(nx-1)^2 + u1(end-1)*(nx-1)/2 ) * ( e*(nx-1)^2 + u2(2)*(nx-1)/2 ) * S1 * S2;
+    g = -( -e*dx^2 + u1(end-1)*dx/2 ) * ( e*dx^2 + u2(2)*dx/2 ) * S1 * S2;
     u2b = ( g*u2bold - u2b )./ (g - 1);
 
     error2_g(iter) = abs(u2b - u2bold);

258fb4d2f562785999fb7fd05b0790a8e576b40b
Conor McCoid <conmccoid@gmail.com>
2018-12-13 11:26:19 +0100

    Initial commit on second version of Newton for tp5, 13.12.18

---
+++ R/newton2.r
@@ -0,0 +1,14 @@
+newton = function(x0,epsi,f,fp){
+  err = abs(f(x0))
+  while (err >= epsi && is.finite(err)){
+    x0 = x0 - f(x0)/fp(x0)
+    err = abs(f(x0))
+  }
+  return(x0)
+}
+
+f = function(x){return(x^3 - 4*x^2 - 1)}
+
+fp= function(x){return(3*x^2 - 8*x)}
+
+fpp = function(x){return(6*x - 8)}
